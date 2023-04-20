4cf83c3d1f49d20f1bac7c2f3cfdca14129f5f77
J. Chitty <jesuschitty7@hotmail.com>
2023-04-20 20:28:21 +0200

    Update sui.sh

--- slim/sui.sh
+++ slim/sui.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$1" = "sui-cli" ]; then
-    export SUI_BINARY=/usr/local/bin/sui
+    export SUI_BINARY=/sui/sui
     shift
     exec "$SUI_BINARY" "$@"
 elif [ "$1" = "sui-tool" ]; then
-    export SUI_TOOL_BINARY=/usr/local/bin/sui-tool
+    export SUI_TOOL_BINARY=/sui/sui-tool
     shift
     exec "$SUI_TOOL_BINARY" "$@"
 else
